1b7461352d12b32662ac8195cb9c06d8087897a8
Braimah-Abiola <braimahabiola.official@gmail.com>
2021-08-01 06:09:39 +0000

    Listing Products

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,38 @@
-import Head from 'next/head'
-import Image from 'next/image'
+import { Card, CardActionArea, CardContent, CardMedia, Button, Grid, Typography, CardActions } from '@material-ui/core'
+import data from '../utils/data'
 import Layout from '../components/Layout'
-import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
     <Layout>
     <div>
       <h1>Products</h1>
-      <ul>
-        <li>Product 1</li>
-      </ul>
+      <Grid container spacing ={3}>
+        {data.products.map((product) => (
+          <Grid item md={4} key={product.name}>
+            <Card>
+              <CardActionArea>
+                <CardMedia 
+                  component="img" 
+                  image={product.image} 
+                  title={product.name}>
+                </CardMedia>
+                <CardContent>
+                  <Typography>
+                    {product.name}
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
+              <CardActions>
+                <Typography>${product.price}</Typography>
+                <Button size="small" color="primary">
+                  Add to cart
+                </Button>
+              </CardActions>
+            </Card>
+          </Grid>
+        ))}
+      </Grid>
     </div>
     </Layout>
   )

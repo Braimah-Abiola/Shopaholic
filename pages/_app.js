8bf1d5c573d0c9f1ace6aadf2d7468ff789679f5
Braimah-Abiola <braimahabiola.official@gmail.com>
2021-08-01 05:41:52 +0000

    Fix Material-UI SSR Issue

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,13 @@
+import { useEffect } from 'react';
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
+  useEffect(()=> {
+      const jssStyles = document.querySelector('#jss-server-siide');
+      if(jssStyles){
+        jssStyles.parentElement.removeChild(jssStyles);
+      }
+  }, []);
   return <Component {...pageProps} />
 }
 

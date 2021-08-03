15c1efa8602d22e76bc8c15a9a6167b10423d27b
Braimah-Abiola <braimahabiola.official@gmail.com>
2021-08-03 22:01:32 +0000

    Add Material UI Theme

--- components/Layout.js
+++ components/Layout.js
@@ -4,13 +4,39 @@ import {
   Link,
   Toolbar,
   Typography,
+  createTheme,
+  ThemeProvider,
+  CssBaseline,
 } from '@material-ui/core';
 import Head from 'next/head';
 import React from 'react';
 import NextLink from 'next/Link';
 import useStyles from '../utils/styles';
 
-export default function Layout({ title, descriptioon, children }) {
+export default function Layout({ title, description, children }) {
+  const theme = createTheme({
+    typography: {
+      h1: {
+        fontSize: '1.6rem',
+        fontWeight: 400,
+        margin: '1rem 0',
+      },
+      h2: {
+        fontSize: '1.4rem',
+        fontWeight: 400,
+        margin: '1rem 0',
+      },
+    },
+    palette: {
+      type: 'light',
+      primary: {
+        main: '#F0c000',
+      },
+      secondary: {
+        main: '#208080',
+      },
+    },
+  });
   const classes = useStyles();
   return (
     <div>
@@ -18,28 +44,31 @@ export default function Layout({ title, descriptioon, children }) {
         <title>{title ? `${title} - Shopaholic` : 'Shopaholic'}</title>
         {description && <meta name="description" content={description}></meta>}
       </Head>
-      <AppBar position="static" className={classes.navbar}>
-        <Toolbar>
-          <NextLink href="/" passHref>
-            <Link>
-              <Typography className={classes.brand}>Shopaholic</Typography>
-            </Link>
-          </NextLink>
-          <div className={classes.grow}></div>
-          <div>
-            <NextLink href="/cart" passHref>
-              <Link>Cart</Link>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <AppBar position="static" className={classes.navbar}>
+          <Toolbar>
+            <NextLink href="/" passHref>
+              <Link>
+                <Typography className={classes.brand}>Shopaholic</Typography>
+              </Link>
             </NextLink>
-            <NextLink href="/login" passHref>
-              <Link>Login</Link>
-            </NextLink>
-          </div>
-        </Toolbar>
-      </AppBar>
-      <Container className={classes.main}>{children}</Container>
-      <footer className={classes.footer}>
-        <Typography>All rights reserved. Shopaholic.</Typography>
-      </footer>
+            <div className={classes.grow}></div>
+            <div>
+              <NextLink href="/cart" passHref>
+                <Link>Cart</Link>
+              </NextLink>
+              <NextLink href="/login" passHref>
+                <Link>Login</Link>
+              </NextLink>
+            </div>
+          </Toolbar>
+        </AppBar>
+        <Container className={classes.main}>{children}</Container>
+        <footer className={classes.footer}>
+          <Typography>All rights reserved. Shopaholic.</Typography>
+        </footer>
+      </ThemeProvider>
     </div>
   );
 }
--- pages/_document.js
+++ pages/_document.js
@@ -1,35 +1,41 @@
+import { Link } from '@material-ui/core';
 import { ServerStyleSheets } from '@material-ui/styles';
-import Document, {Head, Html, Main, NextScript} from 'next/document'
+import Document, { Head, Html, Main, NextScript } from 'next/document';
 import React from 'react';
 
 export default class MyDocument extends Document {
-render(){
+  render() {
     return (
-        <Html lang="en">
-            <Head></Head>
-            <body>
-                <Main/>
-                <NextScript/>
-            </body>
-        </Html>
-    )
-}
+      <Html lang="en">
+        <Head>
+          <Link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
 
 MyDocument.getInitialProps = async (ctx) => {
-    const sheets = new ServerStyleSheets();
-    const originalRenderPage = ctx.renderPage;
-    ctx.renderPage = () => {
-       return originalRenderPage({
-            enhanceApp: (App) => (props) => sheets.collect(<App {...props}/>),
-        });
-    };
-    const initialProps = await Document.getInitialProps(ctx);
-    return {
-        ...initialProps,
-        styles: [
-            ...React.Children.toArray(initialProps.styles),
-            sheets.getStyleElement(), 
-        ],
-    };
+  const sheets = new ServerStyleSheets();
+  const originalRenderPage = ctx.renderPage;
+  ctx.renderPage = () => {
+    return originalRenderPage({
+      enhanceApp: (App) => (props) => sheets.collect(<App {...props} />),
+    });
+  };
+  const initialProps = await Document.getInitialProps(ctx);
+  return {
+    ...initialProps,
+    styles: [
+      ...React.Children.toArray(initialProps.styles),
+      sheets.getStyleElement(),
+    ],
+  };
 };
